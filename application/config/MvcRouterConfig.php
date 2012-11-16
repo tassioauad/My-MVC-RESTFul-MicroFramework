9c42b62724f2fc91066ae1c6dee050df56590aa4
Tássio de O. S. Auad <auadtassio@gmail.com>
2012-11-16 04:49:55 -0200

    new organization of folders

---
+++ application/config/MvcRouterConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace application\config;
+
+abstract class MvcRouterConfig
+{
+    public static $CONTROLLER_PATH = "/src/controller/";
+    public static $VIEW_PATH = "/src/view/";
+}
